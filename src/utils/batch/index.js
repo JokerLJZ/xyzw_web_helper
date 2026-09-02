76311c847d885c3fea7f3d8997fd3a3322fa597a
Stickman.life <Stickman.life@outlook.com>
2026-09-02 12:22:43 +0800

    feat: add camp challenge batch tasks

--- src/utils/batch/index.js
+++ src/utils/batch/index.js
@@ -30,3 +30,4 @@ export { createTasksSalt } from './tasksSalt.js';
 export { createTasksMainLevel } from './tasksMainLevel.js';
 export { createTasksFootball } from './tasksFootball.js';
 export { createTasksApex } from './tasksApex.js';
+export { createTasksCampChallenge } from './tasksCampChallenge.js';
